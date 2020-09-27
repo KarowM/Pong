f26ccb377e296cb16919ff8c5c784589fea91dae
karow <karow.foxx@gmail.com>
2020-09-27 12:41:00 +0100

    added explosion when ball hits wall

--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -7,12 +7,14 @@ public class Wall : MonoBehaviour {
 
     private int score;
     public TextMeshProUGUI scoreText;
+    [SerializeField] GameObject explosion;
     void Start() {
         score = 0;
         scoreText.text = "" + score;
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
+        Instantiate(explosion, collision.transform.position, collision.transform.rotation);
         score++;
         scoreText.text = "" + score;
     }
